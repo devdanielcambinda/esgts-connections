d8eb7bffe03ff1eca3e62ce8ed537c1d8795a11f
Daniel Cambinda <danielcambinda@gmail.com>
2022-07-11 20:10:40 +0100

    deleting routes files

--- back-end-express/src/routers/oportunidade.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const Oportunidade = require('../models/oportunidade')
-const router = new express.Router()
-
-router.post('/', async (req,res) => {
-    
-})
-
-module.exports = router
